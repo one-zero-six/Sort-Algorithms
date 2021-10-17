4ba07bad39c2a6ce3832a59e14b388cc9e069a60
Edel Perez <edel.perez@outlook.com>
2021-10-17 17:54:39 -0400

    Bubble sort animation done

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-function bubbleSort() {
-    const columns = document.querySelectorAll('.bar')
-
-    for (let i = 0; i < columns.length; i++) {
-        for (let index = 0; index < columns.length - i - 1; index++) {
-
-            if (parseFloat(columns[index].style.height) > parseFloat(columns[index + 1].style.height)) {
-                let large = columns[index].style.height;
-                let small = columns[index + 1].style.height;
-
-                columns[index].style.height = small;
-                columns[index + 1].style.height = large;
-            }
-        }
-    }
-}
-
 function reSizing() {
     const columns = Math.floor(visualViewport.width / 7);
 
@@ -31,6 +14,38 @@ function reSizing() {
 }
 
 visualViewport.addEventListener('resize', reSizing);
-document.querySelectorAll('p')[2].addEventListener('click', bubbleSort);
+
+addMoveClass = async (index) => {
+    let columns = document.querySelectorAll('.bar');
+    columns[index].classList.add('bar-moving');
+}
+
+removeMoveClass = async (columns, index) => {
+    columns[index + 1].classList.remove('bar-moving');
+}
+
+codeWait = async () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve()
+        }, 5)
+    })
+}
+
+document.querySelector('#bubbleSort').addEventListener('click', async () => {
+    const columnLengh = document.querySelectorAll('.bar').length;
+
+    for (let i = 0; i < columnLengh; i++) {
+        for (let index = 0; index < columnLengh - i - 1; index++) {
+            let columns = document.querySelectorAll('.bar');
+
+            await codeWait()
+            if (parseFloat(columns[index].style.height) > parseFloat(columns[index + 1].style.height)) {
+                columns[index].insertAdjacentElement('beforebegin', columns[index + 1]);
+            }
+        }
+        await addMoveClass(columnLengh - 1 - i)
+    }
+});
 
 reSizing();
