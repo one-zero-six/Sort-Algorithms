a058f069e4e4dacf5e01a7b863b3a456888db06b
Edel Perez <edel.perez@outlook.com>
2021-10-18 21:12:21 -0400

    insertion sort completed

--- main.js
+++ main.js
@@ -15,20 +15,35 @@ function reSizing() {
 
 visualViewport.addEventListener('resize', reSizing);
 
-addMoveClass = async (index) => {
+const timer = () => {
+    let time = 0
+    let timeDisplay = document.querySelectorAll('.timer');
+
+    setInterval(() => {
+        timeDisplay.innerHTML = ''
+        let minutes = Math.floor(time % 6000);
+        let seconds = Math.floor(time % 1000);
+        time += 1
+        timeDisplay.innerHTML = `timer: ${minutes}:${seconds}.${time}`
+        console.log(minutes, ":", seconds, ".", time)
+    }, 1);
+}
+
+addCompletedClass = async (index) => {
     let columns = document.querySelectorAll('.bar');
     columns[index].classList.add('bar-moving');
 }
 
-removeMoveClass = async (columns, index) => {
-    columns[index + 1].classList.remove('bar-moving');
+removeCompletedClass = async (index) => {
+    let columns = document.querySelectorAll('.bar');
+    columns[index].classList.remove('bar-moving');
 }
 
-codeWait = async () => {
+codeWait = async (delay) => {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve()
-        }, 5)
+        }, delay)
     })
 }
 
@@ -39,12 +54,32 @@ document.querySelector('#bubbleSort').addEventListener('click', async () => {
         for (let index = 0; index < columnLengh - i - 1; index++) {
             let columns = document.querySelectorAll('.bar');
 
-            await codeWait()
             if (parseFloat(columns[index].style.height) > parseFloat(columns[index + 1].style.height)) {
+                await codeWait(10);
+                await addCompletedClass(index)
                 columns[index].insertAdjacentElement('beforebegin', columns[index + 1]);
             }
+            await removeCompletedClass(index)
         }
-        await addMoveClass(columnLengh - 1 - i)
+        await addCompletedClass(columnLengh - 1 - i)
+    }
+});
+
+document.querySelector('#insertionSort').addEventListener('click', async () => {
+    let column = document.querySelectorAll('.bar');
+
+    for (let i = 0; i < column.length - 1; i++) {
+        let j = i;
+
+        while (j >= 0 && parseFloat(column[j].style.height) > parseFloat(column[j + 1].style.height)) {
+            await codeWait(60);
+            await addCompletedClass(j + 1)
+            column[j].insertAdjacentElement('beforebegin', column[j + 1]);
+            await codeWait(60)
+            await removeCompletedClass(j + 1)
+            j -= 1;
+            column = document.querySelectorAll('.bar');
+        }
     }
 });
 
